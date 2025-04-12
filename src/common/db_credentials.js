d305bf1b49e235b104f665640e1c8733ef9afe71
Santhosh20112003 <88790224+Santhosh20112003@users.noreply.github.com>
2025-04-12 15:50:23 +0000

    sample

--- src/common/db_credentials.js
+++ src/common/db_credentials.js
@@ -1,3 +1,7 @@
-export const database_url = "https://tuozwtvqglebbaueohaw.supabase.co";
+// export const database_url = "https://tuozwtvqglebbaueohaw.supabase.co";
+export const database_url = "https://pdnuftqvlbwuatfxouaa.supabase.co";
+// export const database_secret =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1b3p3dHZxZ2xlYmJhdWVvaGF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA4NTY2MjQsImV4cCI6MjAyNjQzMjYyNH0.CfSijV94B0COBgoeiEyXUEvSDLA4txqjwAvR4-LT7u0";
+
 export const database_secret =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1b3p3dHZxZ2xlYmJhdWVvaGF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA4NTY2MjQsImV4cCI6MjAyNjQzMjYyNH0.CfSijV94B0COBgoeiEyXUEvSDLA4txqjwAvR4-LT7u0";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkbnVmdHF2bGJ3dWF0ZnhvdWFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ0Njg3OTUsImV4cCI6MjA2MDA0NDc5NX0.zs0sy_pBuz0AdiHJWh0RKXvgV-6EhNbCJQP088JI9iM";
